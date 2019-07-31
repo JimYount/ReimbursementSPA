abcc87074d246ef48c943e4cf255e568c0456201
Ricky Orr <rlayneorr@gmail.com>
2019-07-31 16:09:40 -0400

    almost everything you need to complete project zero

---
+++ Week2Master/BookStore/src/main/java/com/revature/beans/Genre.java
@@ -0,0 +1,54 @@
+package com.revature.beans;
+
+public class Genre {
+	private Integer id;
+	private String genre;
+	public Genre() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Genre other = (Genre) obj;
+		if (genre == null) {
+			if (other.genre != null)
+				return false;
+		} else if (!genre.equals(other.genre))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Genre [id=" + id + ", genre=" + genre + "]";
+	}
+}
